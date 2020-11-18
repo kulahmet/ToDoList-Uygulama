ba41bf94c35af989c19e49d65fd7d601c3bc200f
Cemil Okay <cemilokay@gmail.com>
2020-11-18 18:06:50 +0000

    Validate if user enters a valid value

--- script.js
+++ script.js
@@ -68,11 +68,13 @@ const cancelItem = function (e) {
 }
 
 const saveItem = function (e) {
+  // Select all the HTML elements
   const textItem = e.target.parentElement.getElementsByClassName('item-text')[0];
   const textInput = e.target.parentElement.getElementsByClassName('item-input')[0];
   const editGroupButtons = e.target.parentElement.getElementsByClassName('btn-group-edit');
   const saveGroupButtons = e.target.parentElement.getElementsByClassName('btn-group-save');
 
+  // Change display preferences of the elements
   textItem.hidden = false;
   textInput.hidden = true;
 
@@ -83,8 +85,14 @@ const saveItem = function (e) {
   for (i of saveGroupButtons) {
     i.hidden = true;
   }
-
-  textItem.innerText = textInput.value;
+  // Assign Text Input's value to Text Item's innerHTML
+  if(textInput.value.trim() === "") {
+    alert('enter a value');
+    // Empty the input value if there are empty spaces
+    document.getElementById('item_name').value = "";
+  } else {
+    textItem.innerText = textInput.value;
+  }
 }
 
 // document.getElementById('btn_add').onclick = addItem;
