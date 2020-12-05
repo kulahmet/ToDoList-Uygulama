9a1b0483c61a2643e138655a8a3c5b601941f679
Cemil Okay <cemilokay@gmail.com>
2020-12-05 09:13:40 +0000

    Add backend implementation

---
+++ frontend/script.js
@@ -0,0 +1,38 @@
+import {addItem, removeItem, editItem, cancelItem, saveItem, completeItem} from './add-item.js'
+
+document.getElementById('btn_add').addEventListener('click', addItem);
+document.getElementById('item_name').addEventListener('keypress', addItem);
+
+for (let i of document.getElementsByClassName('btn-remove')) {
+  i.addEventListener('click', removeItem);
+}
+
+for (let i of document.getElementsByClassName('btn-edit')) {
+  i.addEventListener('click', editItem);
+}
+
+for (let i of document.getElementsByClassName('btn-cancel')) {
+  i.addEventListener('click', cancelItem);
+}
+
+for (let i of document.getElementsByClassName('btn-save')) {
+  i.addEventListener('click', saveItem);
+}
+
+for (let i of document.getElementsByClassName('item-text')) {
+  i.addEventListener('click', completeItem);
+}
+
+// const getResponse = async () => {
+//   try{
+//     const response =  await fetch('http://localhost:8080/');
+//     if(response.ok){
+//       let jsonResponse = await response.json();
+//       console.log(jsonResponse);
+//     }
+//   }
+//   catch(error){
+//     console.log(error);
+//   }
+// }
+// getResponse();
